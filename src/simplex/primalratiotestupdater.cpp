827098da80a1babea6c70fea216bbcf7500bc7bc
smidla <smidla@dcs.uni-pannon.hu>
2013-10-13 14:09:03 +0000

    Nehany cpp file nem volt az svn-hez adva.

---
+++ src/simplex/primalratiotestupdater.cpp
@@ -0,0 +1,6 @@
+#include <simplex/primalratiotestupdater.h>
+
+PrimalRatiotestUpdater::PrimalRatiotestUpdater(IndexList<>* variableFeasibilities):
+    m_variableFeasibilities(variableFeasibilities)
+{
+}
